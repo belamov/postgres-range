f47009b316a3615eb06af03d192a03f1a97c7760
Laravel Shift <shift@laravelshift.com>
2025-03-16 06:24:19 -0400

    Laravel 12.x Compatibility (#30)
    * Bump dependencies for Laravel 12
    
    * Update GitHub Actions for Laravel 12
    
    * fixed grammar constructor
    
    * updated cache action
    
    * update badge
    
    ---------
    
    Co-authored-by: belamov <business@belamov.com>

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd ../docker
-docker-compose run -u 1000 --rm php composer install --no-interaction
-docker-compose run --rm php vendor/bin/phpunit
+docker compose run -u 1000 --rm php composer install --no-interaction
+docker compose run --rm php vendor/bin/phpunit
 cd -
