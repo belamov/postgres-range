03e6c6cde65b40479e6e0912936ab36d0fe1088e
Erko Sten Hürges <erko124@gmail.com>
2025-03-18 15:14:17 +0200

    regex fix

--- src/Casts/TimestampRangeCast.php
+++ src/Casts/TimestampRangeCast.php
@@ -14,4 +14,16 @@ class TimestampRangeCast extends RangeCast
     {
         return new TimestampRange($matches[2], $matches[3], $matches[1], $matches[4]);
     }
+
+    /**
+     * @param  $value
+     * @return array
+     */
+    protected function parseStringRange($value): array
+    {
+        $matches = [];
+        preg_match('/([\[(])\"?([^",]*)\"?,\"?([^",]*)\"?([])])/', $value, $matches);
+
+        return $matches;
+    }
 }
